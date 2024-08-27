ab36f01a69bbaabf73d9382fc87e806da29eea9e
Brent Santschi <brsantsc@a2-r7-p9.s19.be>
2024-08-27 14:39:55 +0200

    Signals

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,11 +6,16 @@
 /*   By: brsantsc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 14:37:07 by brsantsc          #+#    #+#             */
-/*   Updated: 2024/08/26 14:55:10 by brsantsc         ###   ########.fr       */
+/*   Updated: 2024/08/27 13:19:53 by brsantsc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
+//#include "../../includes/minishell.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
 void	handle_sigint(int sig)
 {
@@ -37,9 +42,30 @@ void	sig_handler(void)
 	}
 
 }
+
+void	handle_other_signals(int sig)
+{
+	printf("Error: Caught forbidden signal.\n");
+}
+
+void	sig_err_msg(void)
+{
+	int	sig;
+
+	sig = 1;
+	if (sig != SIGINT || sig != SIGQUIT || sig != SIGKILL)
+	{
+		if(signal(sig, handle_other_signals) == SIG_ERR)
+		{
+			write (STDERR_FILENO, "Error", 5);
+			exit(0);
+		}
+	}
+}
 int	main(void)
 {
 	sig_handler();
+	sig_err_msg();
 	while (1)
 	{
 		//printf("Running... Press Ctrl+C or Ctrl+\\ to test signal handling.\n");
